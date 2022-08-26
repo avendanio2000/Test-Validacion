6509ddf997564797baecdd755a51f35944c18405
Alejandro Avendaño <avendanio2000@hotmail.com>
2022-08-26 12:41:50 -0300

    Agregar archivos de proyecto.

---
+++ Test Validacion/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_Validacion.Models;
+
+namespace Test_Validacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginDataModel logingDataModel)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction("Ok");
+            else
+                return View(logingDataModel);
+        }
+
+        public ActionResult Ok()
+        {
+            //codigo validacion correcta
+            ViewBag.Message = "Inicio exitoso";
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Test Validacion/Models/LoginDataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test_Validacion.Models
+{
+    public class LoginDataModel
+    {
+        [Display(Name = "Correo electronico")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [RegularExpression(@"[a-z0-9]+@[a-z]+\.[a-z]{2,3}", ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        [StringLength(100, ErrorMessage = "Longitud maxima 100")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(15, ErrorMessage = "Longitud entre 6 y 15 caracteres", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme")]
+        public bool RemeberMe { get; set; }
+    }
+}
